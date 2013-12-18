fa7c648c6982d98279e4cbebbe922db5f18783f9
Jan Kowalski <jan.kowalski@allegro.pl>
2013-12-18 17:27:09 +0100

    exercise 7 test #1

--- src/07_async_testing/exercise/timeoutNotify.spec.js
+++ src/07_async_testing/exercise/timeoutNotify.spec.js
@@ -11,6 +11,13 @@ describe('timeout testing', function () {
 
   // slide:start:time;
   it('should send notification after 20 minutes', function () {
+    //given
+    $controller('TimeBoundController', {$scope:$scope});
+   //when
+    $timeout.flush();
+    //then
+    expect($scope.msg).toBe('Your time is up!');
+
   });
   // slide:end;
 

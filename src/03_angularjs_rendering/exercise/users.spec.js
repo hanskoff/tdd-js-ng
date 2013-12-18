a9caeb8c4f1f35590ffdcf95df0bd60958c045ee
Krzysztof Kwiatkowski <krzysztof.kwiatkowski@allegro.pl>
2013-12-18 12:13:07 +0100

    e#03 part 1

--- src/03_angularjs_rendering/exercise/users.spec.js
+++ src/03_angularjs_rendering/exercise/users.spec.js
@@ -1,17 +1,36 @@
 describe('users controller', function () {
 
   var $scope;
-  var usersCtrl;
 
   // slide:start:ctrl;
   beforeEach(inject(function (_$rootScope_, _$controller_) {
+      $scope = _$rootScope_; //.$new();
+
+      usersCtrl = _$controller_('UsersCtrl', {
+        '$scope': $scope
+      });
+
+      //UsersCtrl($scope);
   }));
 
   it('should initialize scope with an empty users collection', function () {
+      expect($scope.cleanUser).toEqual({});
+      expect($scope.users.length).toEqual(0);
   });
   // slide:end
 
   it('should save a current user and update users list', function () {
+    // given
+    $scope.user = {
+      name: 'Pawel'
+    };
+
+    // when
+    $scope.save();
+
+    // then
+    expect($scope.users.length).toEqual(1);
+    expect($scope.users[0].name).toEqual('Pawel');
   });
 
   it('should dirty check and allow clearing user edits', function () {

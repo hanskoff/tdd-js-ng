827bbc02490ac2c30938b71c1778361d9845ae36
Maciej Zasada <maciej.zasada@allegro.pl>
2013-12-18 14:18:53 +0100

    e#04 part 1

--- src/04_modules_and_di/exercise/users.js
+++ src/04_modules_and_di/exercise/users.js
@@ -1,6 +1,36 @@
 //slide:start:ctrl;
-angular.module('users', [])
+angular.module('users', ['userstorage'])
 
-  .controller('UsersCtrl', function ($scope, UserStorage) {
+    .controller('UsersCtrl', function ($scope, userStorage) {
 
-  });
+        $scope.cleanUser = {};
+        $scope.users = userStorage.getAll();
+
+        $scope.save = function () {
+            userStorage.save($scope.user);
+            $scope.cleanUser = {};
+            $scope.clear();
+            $scope.users = userStorage.getAll();
+        };
+
+        $scope.remove = function (userId) {
+            userStorage.remove(userId);
+            $scope.users = userStorage.getAll();
+        };
+
+        $scope.edit = function (user) {
+            $scope.cleanUser = angular.copy(user);
+            $scope.clear();
+        };
+
+        $scope.hasEdits = function () {
+            return !angular.equals($scope.user, $scope.cleanUser);
+        };
+
+        $scope.clear = function () {
+            $scope.user = angular.copy($scope.cleanUser);
+        };
+
+        $scope.clear();
+
+    });
--- src/04_modules_and_di/exercise/userstorage.js
+++ src/04_modules_and_di/exercise/userstorage.js
@@ -1,14 +1,43 @@
 angular.module('userstorage', [])
+    .factory('UserStorage', function () {
 
-  .factory('UserStorage', function () {
+        var users = {};
 
-    var users = {};
+        // Sequence can now be encapsulated (was exposed on a constructor function before)
+        var sequence = 1;
 
-    // Sequence can now be encapsulated (was exposed on a constructor function before)
-    var sequence = 1;
+        // AngularJS services are singletons
+        var UserStorage = {};
 
-    // AngularJS services are singletons
-    var UserStorage = {};
+        UserStorage.save = function (user) {
+            if (!user.id) {
+                user.id = sequence++;
+            }
+            users[user.id] = user;
 
-    return UserStorage;
-  });
+            return user;
+        };
+
+        UserStorage.remove = function (userId) {
+            var toBeDeleted = this.getById(userId);
+            if (toBeDeleted) {
+                delete users[userId];
+            }
+
+            return toBeDeleted;
+        };
+
+        UserStorage.getById = function (userId) {
+            return users[userId] || null;
+        };
+
+        UserStorage.getAll = function () {
+            var result = [];
+            for (var userId in users) {
+                result.push(users[userId]);
+            }
+            return result;
+        };
+
+        return UserStorage;
+    });
